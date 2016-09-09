664a209f14e7e420b57e9ed436814ea6d31b49bb
Shankar Kulumani SEAS <shanks.k@gmail.com>
2016-09-09 14:10:08 -0400

    Fix kinetic energy computation

--- foucault_driver.m
+++ foucault_driver.m
@@ -5,7 +5,7 @@ clearvars
 clc
 close all
 
-% define constants
+%% define constants
 constants.Omega = 7.2921158553e-5; % rad/sec earth angular velocity
 constants.mu = 3.986004418e14; % m^3/sec
 % % original Foucault Pendulum
@@ -22,13 +22,20 @@ constants.Cbeta = [cos(constants.beta)^2                    0   -sin(constants.b
                    -sin(constants.beta)*cos(constants.beta) 0   sin(constants.beta)^2];
 constants.S = hat_map(constants.Omega*(ROT2(constants.beta)')'*[0;0;1]); % my ROT matrix assumes row notation
 
-% simulation parameters
+m = constants.m;
+Len = constants.L;
+S = constants.S;
+Omega = constants.Omega;
+Cbeta = constants.Cbeta;
+Re = constants.Re;
+
+%% simulation parameters
 tspan = [0:0.01:100]; % seconds
 pos_initial = ROT3(45*pi/180)'*[-1;0;0];
 initial_condition = [pos_initial;0;0;0];
 ode_options = odeset('RelTol',1e-9,'AbsTol',1e-9);
 
-% call ODE
+%% call ODE
 [t,state] = ode113(@(t,state)foucault_ode(t,state,constants),tspan,initial_condition,ode_options);
 % [t,state] = ode113(@(t,state)foucault_ode_length(t,state,constants),tspan,initial_condition,ode_options);
 
@@ -43,11 +50,15 @@ E = zeros(length(t),1);
 
 % calculate total energy
 for ii = 1:length(t)
-    T(ii) = 1/2*constants.m*norm(vel(ii,:));
-%     V(ii) = - constants.m*constants.mu / norm(constants.Re*[1;0;0] + constants.L*pos(ii,:)');
-    V(ii) = - constants.m*constants.g*constants.Re^2 / norm(constants.Re*[1;0;0] + constants.L*pos(ii,:)');
-%     V(ii) = -constants.m*constants.g*constants.L*pos(ii,1);
+    body_pos = constants.Re*[1;0;0]+constants.L*pos(ii,:)';
     
+    % need the kinetic energy in the inertial frame
+    T(ii) = 1/2*m*Len^2*norm(vel(ii,:))^2 + ...
+            m*Len*vel(ii,:)*S*body_pos + ...
+            1/2*m*Omega^2*body_pos'*Cbeta*body_pos; 
+
+    V(ii) = - constants.m*constants.g*constants.Re^2 / norm(body_pos);
+
     L(ii) = T(ii)-V(ii);
     E(ii) = T(ii)+V(ii);
     
@@ -56,7 +67,7 @@ end
 % energy variation
 E_diff = abs(E - E(1)); 
 
-% plot outputs
+%% plot outputs
 fontsize = 18;
 fontname = 'Times';
 
