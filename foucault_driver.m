3ca96640f2605d5c50948df748427d3164385b46
Shankar Kulumani SEAS <shanks.k@gmail.com>
2016-09-30 13:40:17 -0400

    Nonplanar test 1
    pos_initial = ROT2(90*pi/180)*[-1;0;0];
    vel_initial = [0;0.1;0];

--- foucault_driver.m
+++ foucault_driver.m
@@ -7,8 +7,8 @@ close all
 
 %% define constants
 constants.eom = 'full'; % full or len or rot for simplifications
-constants.Omega = 7.2921158553e-5; % rad/sec earth angular velocity
-% constants.Omega = 2*pi/(86400/24);
+% constants.Omega = 7.2921158553e-5; % rad/sec earth angular velocity
+constants.Omega = 7.2921158553e-5;
 constants.mu = 3.986004418e14; % m^3/sec
 % % original Foucault Pendulum
 % constants.L = 67; % meters
@@ -16,9 +16,10 @@ constants.mu = 3.986004418e14; % m^3/sec
 % constants.beta = 48.846222*pi/180; % Latitude for the Pantheon, Paris 
 constants.L = 100;
 constants.m = 100;
-constants.beta = 25*pi/180; % latitude of pivot location on Earth
+constants.beta = 40*pi/180; % latitude of pivot location on Earth
 constants.Re = 6378.137 * 1e3; % meters radius of the Earth
-constants.g = 9.7976432222; % mean g at equator in meters/sec^2
+% constants.g = 9.7976432222; % mean g at equator in meters/sec^2
+constants.g = 9.7976432222;
 constants.Cbeta = [cos(constants.beta)^2                    0   -sin(constants.beta)*cos(constants.beta);...
                    0                                        1              0                          ;...
                    -sin(constants.beta)*cos(constants.beta) 0   sin(constants.beta)^2];
@@ -32,9 +33,9 @@ Cbeta = constants.Cbeta;
 Re = constants.Re;
 
 %% simulation parameters
-tspan = [0:0.1:100]; % seconds
-pos_initial = ROT2(45*pi/180)*[-1;0;0];
-vel_initial = [0;0;0];
+tspan = [0:0.1:200]; % seconds
+pos_initial = ROT2(90*pi/180)*[-1;0;0];
+vel_initial = [0;0.1;0];
 
 initial_condition = [pos_initial;vel_initial];
 ode_options = odeset('RelTol',1e-9,'AbsTol',1e-9);
@@ -60,6 +61,9 @@ pend_pos_full = constants.L*pos_full; % location of mass in body frame
 %% Plot the outputs
 % plot_outputs(t_full,pos_full,vel_full,constants) % without L applied (direction only)
 
+type = 'gif';
+filename = 'pend';
+
 % animation
-% body_animation(t_full,pos_full,vel_full,constants)
-inertial_animation(t_full,pos_full,vel_full,constants)
+body_animation(t_full,pos_full,vel_full,constants,type,strcat(filename,'_body'))
+% inertial_animation(t_full,pos_full,vel_full,constants,type,strcat(filename,'_inertial'))
