7d8fc234163fd07e43fea011b65885a72e11e5f5
Shankar Kulumani SEAS-Linux <skulumani@gwu.edu>
2016-10-28 16:33:00 -0400

    Use mat/vec instead of matrix/vector

--- rot/dcm.py
+++ rot/dcm.py
@@ -1,40 +1,40 @@
 import numpy as np
 
-def hat_map(vector):
+def hat_map(vec):
     """Linear map from R^3 to so(3)
 
     Inputs:
-        - vector - 3x1 numpy array
+        - vec - 3x1 numpy array
 
     Outputs:
-        - matrix - 3x3 skew-symmetric numpy array
+        - mat - 3x3 skew-symmetric numpy array
     """
 
-    matrix = np.zeros(3,3);
+    mat = np.zeros([3,3])
+    
+    mat[0,1] = -vec[2]
+    mat[0,2] = vec[1]
+    mat[1,0] = vec[2]
+    mat[1,2] = -vec[0]
+    mat[2,0] = -vec[1]
+    mat[2,1] = vec[0]
 
-    matrix(0,1) = -vector(2);
-    matrix(0,2) = vector(1);
-    matrix(1,0) = vector(2);
-    matrix(1,2) = -vector(0);
-    matrix(2,0) = -vector(2);
-    matrix(2,1) = vector(0);
+    return mat
 
-    return matrix
-
-def vee_map(matrix):
+def vee_map(mat):
     """Linear map from so(3) to R^3
 
     Inputs:
-        - matrix - 3x3 numpy array
+        - mat - 3x3 numpy array
 
     Outputs:
-        - vector - 3x1 numpy array
+        - vec - 3x1 numpy array
     """
 
-    x1 = matrix(2,1)-matrix(1,2);
-    x2 = matrix(0,2) - matrix(2,0);
-    x3 = matrix(1,0)-matrix(0,1);
+    x1 = mat[2,1]-mat[1,2]
+    x2 = mat[0,2] - mat[2,0]
+    x3 = mat[1,0]-mat[0,1]
 
-    vector = 1.0/2*[x1;x2;x3];
+    vec = 1.0/2*np.array([x1,x2,x3])
 
-    return vector
+    return vec
